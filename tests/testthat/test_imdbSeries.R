e11b4da716d50b2be46cad3881bc69198ac611f2
Roel Hogervorst <hogervorst.rm@gmail.com>
2018-04-10 13:03:54 +0200

    complete change initiated by Josh Yazman
    adds tests, modifies description to include Josh's name
    Also modifies all the functions to include the api keyword
    
    This change addresses the need by:
    
    Everyone, because the omdb api has changed and requires a key.

--- tests/testthat/test_imdbSeries.R
+++ tests/testthat/test_imdbSeries.R
@@ -1,20 +1,20 @@
 context("imdbSeries characters")
 
-
+key1 <- Sys.getenv('OMDB_KEY')
 test_that("several caps and types return identical frames", {
-        a<- imdbSeries("Game OF THRONES")
-        b<- imdbSeries("Game-of-thrones")
+        a<- imdbSeries("Game OF THRONES", key = key1)
+        b<- imdbSeries("Game-of-thrones", key = key1)
         expect_equal(a,b)
 })
 
 test_that("usefull error messages are created", {
         skip_on_travis()
-        expect_error(imdbSeries("Game OF THRONES", seasons = 1-5),
+        expect_error(imdbSeries("Game OF THRONES", seasons = 1-5, key = key1),
                      regexp = "season numbers")
 })
 
-sample<-  imdbSeries("Game OF THRONES", seasons = 3)
-sample_2 <- enrichIMDB(sample)
+sample<-  imdbSeries("Game OF THRONES", seasons = 3, key = key1)
+sample_2 <- enrichIMDB(sample, key = key1)
 test_that("enrichment works", {
         expect_match(sample$Title[5], "Kissed by Fire")
         expect_match(sample_2$plot[5], "Jon breaks his vows.")
